331d26a1263c4dced70a0dbcd480e03d591c2a63
unknown <mpayimanaemilien@gmail.com>
2023-09-01 01:30:42 +0300

    pro1

--- app/api/shaka1/search1/route.js
+++ app/api/shaka1/search1/route.js
@@ -38,7 +38,7 @@ export async function GET(request) {
       method: 'GET',
       headers: {
         "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Origin": "*", // Replace with your allowed origins
+        "Access-Control-Allow-Origin": "https://rangaone-icyangombwa-4qpnpqfzn-emilienmpaye.vercel.app", // Replace with your allowed origins
         "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
         "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     }
