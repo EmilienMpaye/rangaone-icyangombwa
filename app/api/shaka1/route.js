dc046e8d20685c98d81548fc9a0941c9287d9b91
unknown <mpayimanaemilien@gmail.com>
2023-09-01 14:53:29 +0300

    pro1

--- app/api/shaka1/route.js
+++ app/api/shaka1/route.js
@@ -34,7 +34,7 @@ export async function GET() {
       headers: {
         'Content-Type': 'application/json',
                "Access-Control-Allow-Credentials": "true" ,
-               "Access-Control-Allow-Origin": "http://localhost:3000", // Replace with your allowed origins
+               "Access-Control-Allow-Origin": "https://rangaone-icyangombwa.vercel.app", // Replace with your allowed origins
                "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT" ,
               "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" ,    
       }
