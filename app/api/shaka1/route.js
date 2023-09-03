5b7b03692d8635fa883edad1f7052ca3600426cc
unknown <mpayimanaemilien@gmail.com>
2023-09-03 12:34:11 +0300

    pro1

--- app/api/shaka1/route.js
+++ app/api/shaka1/route.js
@@ -54,8 +54,8 @@ export async function GET( request) {
         cache: 'no-store' ,
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json'
-
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": origin || "https://rangaone-icyangombwa.vercel.app",
       }
     });
     console.log("shaka1  route",response);
