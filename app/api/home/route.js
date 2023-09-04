f8c67c604819de320816beccbae8b8384262f002
unknown <mpayimanaemilien@gmail.com>
2023-09-04 09:47:01 +0300

    pro1

--- app/api/home/route.js
+++ app/api/home/route.js
@@ -1,8 +1,17 @@
- import { NextResponse } from "next/server";
- import { getAllTodos } from "@/lib/mongo/todos";
+import {  NextResponse } from "next/server";
+import { getAllTodos} from "@lib/mongo/todos";
 
-  export async function GET() {
+export async function GET(context){
+   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
    const todos = await getAllTodos();
-   return NextResponse.json(todos);
-  }
 
+   const response = NextResponse.json(todos);
+  
+  // Add headers to the response.
+  response.headers.set('Access-Control-Allow-Origin', 'https://rangaone-icyangombwa.vercel.app');
+ 
+  return response;
+   //return NextResponse.json(todos1);
+   
+}
+    
--- app/api/shaka1/route.js
+++ app/api/shaka1/route.js
@@ -4,41 +4,6 @@
 
 
 
-// export async function GET(request) {
-//   const origin =request.headers.get('origin');
-//   console.log("origin",origin);
-//   const apishaka1 =process.env.API_HOME1_CACHEBUSTER_URL;
-//     //const cacheBuster = Date.now();//API_HOME1_CACHEBUSTER_URL=https://rangaone-icyangombwa.vercel.app/api/home1?cacheBuster=
-//     //const urlWithCacheBuster = `${apishaka1}${cacheBuster}`;
-//     console.log("shaka1  api",apishaka1);
-//     const response = await fetch(apishaka1, {
-//         cache: 'no-store' ,
-      
-//       method: 'GET',
-//       headers: {
-//       //   'Content-Type': 'application/json',
-//       //          "Access-Control-Allow-Credentials": "true" ,
-//                "Access-Control-Allow-Origin": origin|| "*", // Replace with your allowed origins
-//       //          "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT" ,
-              
-//         }
-//     });
-//     console.log("shaka1  route",response);
-//     const {todos1} = await response.json();
-//     console.log("rour",todos1);
-  
-    
-//       return NextResponse .json({
-//         todos1,
-//         revalidate:5
-// }
-//         ,{
-//           headers: {
-//             'Access-Control-Allow-Origin': origin || "*",
-//             'Content-Type': 'application/json'
-//           }
-//         }
-//       );
 //       }
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
