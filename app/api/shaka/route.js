71359a6a9997aab8956cd9d207903c02182c13fa
unknown <mpayimanaemilien@gmail.com>
2023-09-05 12:44:36 +0300

    pro1

--- app/api/shaka/route.js
+++ app/api/shaka/route.js
@@ -1,12 +1,12 @@
  import {NextResponse} from "next/server";
  export const dynamic = "force-dynamic";
 // //API_HOME_CACHEBUSTER_URL=http://localhost:3000/api/home?cacheBuster=
-        
+ const homeapi =process.env.API_HOME_URL       
 export async function GET() {
-  const apisearch = process.env.API_HOME_CACHEBUSTER_URL;
+  //const apisearch = process.env.API_HOME_CACHEBUSTER_URL;
  // const cacheBuster = Date.now();
     //const urlWithCacheBuster = `${apisearch}${cacheBuster}`;
-    const response = await fetch('http://localhost:3000/api/home',{
+    const response = await fetch(homeapi,{
       next: {
         cache: 'no-cache',
         revalidate: 0
