42deab1711b59da1f93b79aec2362c3fed127313
unknown <mpayimanaemilien@gmail.com>
2023-09-05 12:00:31 +0300

    pro1

--- app/api/home/route.js
+++ app/api/home/route.js
@@ -2,16 +2,19 @@ import {  NextResponse } from "next/server";
 import { getAllTodos} from "@lib/mongo/todos";
 
 export async function GET(context){
-   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
    const todos = await getAllTodos();
 
-   const response = NextResponse.json(todos);
-    //console.log("home data",response)
-  // Add headers to the response.
+   const response = NextResponse.json(todos,{
+      next: {
+        cache: 'no-cache',
+        revalidate: 5
+      }
+    });
+    
   response.headers.set('Access-Control-Allow-Origin', 'https://rangaone-icyangombwa.vercel.app');
  
   return response;
-   //return NextResponse.json(todos1);
+   
    
 }
     
--- app/api/shaka/route.js
+++ app/api/shaka/route.js
@@ -1,25 +1,21 @@
  import {NextResponse} from "next/server";
  export const dynamic = "force-dynamic";
 // //API_HOME_CACHEBUSTER_URL=http://localhost:3000/api/home?cacheBuster=
-
-
-// async function fetchIbya(){
-    
-//     const cacheBuster = Date.now(); // Generate a `http://localhost:3000/api/home?cacheBuster=${cacheBuster}` cache-busting parameter
-//     //const urlWithCacheBuster = `${apisearch}${cacheBuster}`;
-   
-
         
-
 export async function GET() {
   const apisearch = process.env.API_HOME_CACHEBUSTER_URL;
   const cacheBuster = Date.now();
-    const urlWithCacheBuster = `${apisearch}${cacheBuster}`;
-    const response = await fetch(urlWithCacheBuster)
+    //const urlWithCacheBuster = `${apisearch}${cacheBuster}`;
+    const response = await fetch(apisearch,{
+      next: {
+        cache: 'no-cache',
+        revalidate: 5
+      }
+    })
       
     const {todos} = await response.json();
     console.log("rour",todos);
-    const data = NextResponse.json({todos,revalidate:4})
+    const data = NextResponse.json({todos})
     data.headers.set('Access-Control-Allow-Origin', 'https://rangaone-icyangombwa.vercel.app')
     
      return data 
