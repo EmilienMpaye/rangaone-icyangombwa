23e957859c4f215ab554d880683fc5ea2692e186
unknown <mpayimanaemilien@gmail.com>
2023-09-06 09:52:00 +0300

    pro1

--- app/api/home1/route.js
+++ app/api/home1/route.js
@@ -4,9 +4,9 @@ import { getAllTodos1} from "@lib/mongo/todos1";
 export async function GET(context){
    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
    const todos1 = await getAllTodos1();
-
+ console.log("todos API",todos1)
    const response = NextResponse.json(todos1,{cache: 'no-cache'});
-   console.log("home1 data",response)
+   console.log("home1 data API",response)
   // Add headers to the response.
   response.headers.set('Access-Control-Allow-Origin', 'https://rangaone-icyangombwa.vercel.app/api/home1');
  
