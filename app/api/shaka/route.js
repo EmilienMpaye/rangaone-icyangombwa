f282725a40f6f4554fa552a742a2375103c81210
unknown <mpayimanaemilien@gmail.com>
2023-09-06 10:52:02 +0300

    pro1

--- app/api/shaka/route.js
+++ app/api/shaka/route.js
@@ -1,12 +1,13 @@
  import {NextResponse} from "next/server";
 export const dynamic = "force-dynamic";
-// //API_HOME_CACHEBUSTER_URL=http://localhost:3000/api/home?cacheBuster=
+// //API_HOME_CACHEBUSTER_URL=http://localhost:3000/api/home
       
 export async function GET() {
   const homeapi =process.env.API_HOME_URL 
   //const apisearch = process.env.API_HOME_CACHEBUSTER_URL;
  // const cacheBuster = Date.now();
     //const urlWithCacheBuster = `${apisearch}${cacheBuster}`;
+    
     const response = await fetch(homeapi,{
       next: {
         cache: 'no-cache',
@@ -14,10 +15,13 @@ export async function GET() {
       }
     })
       
-    const {todos} = await response.json();
-    console.log("rour",todos);
-    const data = NextResponse.json({todos})
-    data.headers.set('Access-Control-Allow-Origin', 'https://rangaone-icyangombwa.vercel.app')
-    
-     return data 
+    if (response.ok) {
+      const { todos } = await response.json();
+      const data = NextResponse.json({ todos });
+      return data;
+    } else {
+      // Handle error here
+      console.error('Failed to fetch data from API');
+      return new Response(null, { status: 500 }); // Return an appropriate response for the error
+    }
   }
