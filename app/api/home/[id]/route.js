f2018f047922699eb061b18f9c86e7fb3d469f18
unknown <mpayimanaemilien@gmail.com>
2023-09-06 19:27:59 +0300

    pro1

--- app/api/home/[id]/route.js
+++ app/api/home/[id]/route.js
@@ -14,7 +14,8 @@ export const GET = async(request,{params}) =>{
             return NextResponse.json(
                 {message:"todos not found",error},{status:404})
         }
-                 
+        
+        
    return NextResponse.json(todos);
     } catch(error){
         return NextResponse.json({message :"Get an error",error},{status:500});
@@ -36,10 +37,24 @@ export const DELETE = async (request, { params }) => {
         );
       }
   
-      return NextResponse.json(
+
+      const response = NextResponse.json(
         { message: `Todo with ID ${id} deleted successfully` },
-        { status: 200 }
-      );
+         { status: 200 },{
+        next: {
+          cache: 'no-cache'
+        }
+      });
+      
+      response.headers.set('Access-Control-Allow-Origin', 'https://rangaone-icyangombwa.vercel.app');
+      response.headers.set('Cashe-Control','s-maxage=5, must-revalidate');
+      response.headers.set('X-Content-Type-Options', 'nosniff');
+      
+      // return NextResponse.json(
+      //   { message: `Todo with ID ${id} deleted successfully` },
+      //   { status: 200 }
+      // );
+      return response
     } catch (error) {
       console.error("Failed to delete todo:", error);
       return NextResponse.json(
